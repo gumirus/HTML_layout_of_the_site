391a4b49b3e4355459243cdf837af13348e5ceb4
Ruslan Gumirov <gumirus@yandex.ru>
2023-08-25 06:53:52 +0500

    Init gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -29,3 +29,14 @@ gulp.task('server:dev', function () {
         }
     });
 });
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
