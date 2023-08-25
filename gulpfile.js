e3a4ba69a4d5f4d4fc15d513a1e0e44c79a25e52
Ruslan Gumirov <gumirus@yandex.ru>
2023-08-25 10:21:31 +0500

    corrections

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const deploy = require('gulp-gh-pages');
-const ghPages = require('gh-pages'); // Добавляем gh-pages
+const ghPages = require('gh-pages'); 
 
-// Tasks
+
 require('./gulp/dev.js');
 require('./gulp/docs.js');
 
@@ -24,19 +24,19 @@ gulp.task(
 	)
 );
 
-// Объединяем задачи deploy в одну
+
 gulp.task('deploy', function (cb) {
-  // Замените на имя вашего репозитория и путь к сборке вашего проекта
+
   const deployPath = './build';
 
-  // Развертываем на GitHub Pages с использованием gulp-gh-pages
+
   return gulp.src(deployPath + '/**/*')
     .pipe(deploy({
       remoteUrl: "https://github.com/gumirus/HTML_layout_of_the_site.git",
       branch: "gh-pages"
     }))
     .on('error', function(err) {
-      console.log(err); // Выводим ошибки в консоль, если есть
-      cb(err); // Прерываем выполнение задачи при ошибке
+      console.log(err); 
+      cb(err); 
     });
 });
